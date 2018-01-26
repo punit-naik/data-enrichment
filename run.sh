32a1602d6bd33aae03ae3f7c7e695f7817925183
Punit Naik <naik.punit44@gmail.com>
2018-01-26 11:18:44 +0530

    :art: Improved argument checking

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ echo -e "4 -> Furniture"
 echo -e "5 -> Smartphones"
 echo -e
 read -p 'Please select a category: ' category
-if [ "$category" -ge 1 -a "$category" -le 5 ]
+if [[ "$category" =~ ^-?[0-9]+$ ]] && [ "$category" -ge 1 -a "$category" -le 5 ]
     then
         java -jar target/data-enrichment-0.1.0-SNAPSHOT-standalone.jar $category;
 else
